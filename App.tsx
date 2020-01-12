b5a50a4c28171384cf83d0f7b71f421bea6d0e52
starodubenko <r.starodubenko@tinkoff.ru>
2020-01-13 01:24:08 +0300

    Initial commit with redux-persist

---
+++ App.tsx
@@ -0,0 +1,52 @@
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default function App() {
+//   return (
+//
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <Text>Navigation</Text>
+//       <Text>Deep linking</Text>
+//       <Text>Photo/Video</Text>
+//       <Text>Push notifications/Custom android/ios</Text>
+//       <Text>Animations</Text>
+//     </View>
+//   );
+// }
+
+// Imports: Dependencies
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+// Imports: Screens
+import Counter from './src/screens/Counter';
+// Imports: Redux Persist Persister
+import { store, persistor } from './src/store';
+// React Native: App
+export default function App() {
+    return (
+        // Redux: Global Store
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Counter />
+                <Text>Redux dev tools</Text>
+                <Text>Navigation</Text>
+                <Text>Deep linking</Text>
+                <Text>Photo/Video</Text>
+                <Text>Push notifications/Custom android/ios</Text>
+                <Text>Animations</Text>
+            </PersistGate>
+        </Provider>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
